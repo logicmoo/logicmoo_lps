631723a12896a44820f6d223ce1243688e5185ca
Douglas R. Miles <logicmoo@gmail.com>
2020-10-13 14:12:14 -0700

    % in case :- current_predicate(swish:is_a_module/0) -> true ; asserta(swish:is_a_module).

--- prolog/lps_corner.pl
+++ prolog/lps_corner.pl
@@ -1,6 +1,5 @@
 
 :- module(lps_corner,[golps/1,golps/3]).
-:- asserta(swish:is_a_module).
 
 %:- system:use_module(library('../engine/db.P')).
 :- module_transparent(golps/1).
@@ -19,6 +18,7 @@ golps(T,DFAgraph,Options) :-
 
 
 :- user:ensure_loaded(library(dialect/lps)).
+:- current_predicate(swish:is_a_module/0) -> true ; asserta(swish:is_a_module).
 :- interpreter:use_module(library('../engine/interpreter.P')).
 :- lps_term_expander:use_module(library('../swish/term_expander')).
 :- visualizer:use_module(library('../utils/visualizer.P')).
